6f0a5cd9fa7d5b6c8eab01cf93c7d40d30841b5f
Виталий Набережный <naberezhniy@taxi21.ru>
2017-09-29 12:40:18 +0300

    finish

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,25 @@ import "../css/main.scss";
 (function($) {
     $(function() {
 
-        $('.button-collapse').sideNav();
+        // $('.button-collapse').sideNav();
         $('.parallax').parallax();
-
         $("#copy-date").text(new Date().getFullYear());
-        var phoneMask = new IMask(
-            document.getElementById('phone'), {
-                mask: '+{7} (000) 000-00-00'
-            });
+
+        var phoneMask = new IMask(document.getElementById('phone'), {
+            mask: '+{7} (000) 000-00-00'
+        });
+
+        var tapTarget = setTimeout(function() {
+            $('.tap-target').tapTarget('open');
+        }, 10000);
+
+        Materialize.scrollFire([{
+            selector: '.scroll-spy',
+            offset: 200,
+            callback: function() {
+                clearTimeout(tapTarget);
+                $('.tap-target').tapTarget('close');
+            }
+        }]);
     }); // end of document ready
 })(jQuery); // end of jQuery name space
