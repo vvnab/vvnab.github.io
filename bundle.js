2e66c87461dcca67cd5d276347a1cc773454c1fd
Виталий Набережный <naberezhniy@taxi21.ru>
2017-10-04 17:18:15 +0300

    --

--- bundle.js
+++ bundle.js
@@ -106,8 +106,51 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 }, 5000);
             }
         }]);
+
+        function postAjax(url, data, success) {
+            const params = JSON.stringify(data);
+            let xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+            xhr.open('POST', url);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState > 3 && xhr.status == 200) {
+                    success(xhr.responseText);
+                }
+            };
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(params);
+            return xhr;
+        }
+
+        $("#send-email").click(
+            function() {
+                var name = ($("#name").val() == "" || $("#name").hasClass("invalid")) ? null : $("#name").val();
+                var phone = ($("#phone").val() == "" || $("#phone").hasClass("invalid")) ? null : $("#phone").val();
+                var email = ($("#email").val() == "" || $("#email").hasClass("invalid")) ? null : $("#email").val();
+
+                if (!name || !phone || !email) {
+                    console.error("form error")
+                } else {
+
+                    var message = {
+                        from: "job@vvnab.ru",
+                        to: "vvnab@mail.ru",
+                        subject: `Заказ сайта`,
+                        text: `
+Имя: ${name}
+Телефон: ${phone}
+E-Mail: ${email}
+`
+                    };
+                    postAjax("https://api-driver.taxi21.ru/utils/mail?mail-access-key=123456", message, result => {
+                        console.log("OK", name, phone, email);
+                    });
+
+                }
+            }
+        )
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
 
 /***/ }),
 /* 1 */
